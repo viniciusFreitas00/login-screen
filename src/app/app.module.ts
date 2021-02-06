ac732ab0a2a00faea66c456bedb72e3ae086fd49
vinicius freitas <vini.freirefreitas00@gmail.com>
2021-02-06 02:13:01 -0300

    :lipstick: creating login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginCardComponent } from './login-card/login-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginCardComponent
   ],
   imports: [
     BrowserModule
