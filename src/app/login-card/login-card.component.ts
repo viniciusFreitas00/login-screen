9ed7b269f8f5d3c6eff5083e9eb771da49a566d9
vinicius freitas <vini.freirefreitas00@gmail.com>
2021-02-07 20:41:34 -0300

    :lipstick: add input animation

--- src/app/login-card/login-card.component.ts
+++ src/app/login-card/login-card.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login-card',
   templateUrl: './login-card.component.html',
-  styleUrls: ['./login-card.component.css']
+  styleUrls: ['./login-card.component.css'],
 })
 export class LoginCardComponent implements OnInit {
+  @ViewChild('email', { static: false }) email: ElementRef;
+  @ViewChild('email_label', { static: false }) email_label: ElementRef;
+  @ViewChild('password', { static: false }) password: ElementRef;
+  @ViewChild('password_label', { static: false }) password_label: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  public handleInputBlur(event: any): void {
+    let aux = event.target.id;
+    let classAux: string;
+    if (aux == 'email') {
+      classAux =
+        this.email.nativeElement.value.trim() == '' ? '' : 'label_selected';
+      this.email_label.nativeElement.classList = classAux;
+    } else {
+      classAux =
+        this.password.nativeElement.value.trim() == '' ? '' : 'label_selected';
+      this.password_label.nativeElement.classList = classAux;
+    }
   }
 
+  ngOnInit(): void {}
 }
